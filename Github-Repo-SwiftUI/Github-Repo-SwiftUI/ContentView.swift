951e8ee764e487deeb3e716b9caca9872787d15a
ankit bharti <ankitbharti1994@gmail.com>
2019-11-06 22:36:12 +0530

    Initial commit

---
+++ Github-Repo-SwiftUI/Github-Repo-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github-Repo-SwiftUI
+//
+//  Created by ankit bharti on 06/11/19.
+//  Copyright © 2019 ankit kumar bharti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
