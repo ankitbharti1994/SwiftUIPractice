f0b505ff2056a24b7e4c1675ddc834ef51706a7b
ankit bharti <ankitbharti1994@gmail.com>
2019-12-18 21:23:41 +0530

    practice for onReceive method on view

--- Refreshing/Refreshing/ContentView.swift
+++ Refreshing/Refreshing/ContentView.swift
@@ -7,15 +7,64 @@
 //
 
 import SwiftUI
+import Combine
+
 
 struct ContentView: View {
+    @State private var isDevelopingMacOSApp = false
+    @State private var message = ""
+    private let model = Model()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            Text(self.isDevelopingMacOSApp ? "MacOS Developement" : "iOS Development")
+                .font(.largeTitle)
+            CustomButton(isDevelopingMacOSApp: $isDevelopingMacOSApp)
+            Text(message)
+        }
+        .onReceive(self.model.value.receive(on: DispatchQueue.main)) { isPublished in
+            self.isDevelopingMacOSApp = isPublished
+        }
+        .onReceive(self.model.message.receive(on: DispatchQueue.main)) { message in
+            self.message = message
+        }
     }
 }
 
+struct CustomButton: View {
+    @Binding var isDevelopingMacOSApp: Bool
+    var body: some View {
+        Button(action: {
+            self.isDevelopingMacOSApp.toggle()
+        }) {
+            Text(self.isDevelopingMacOSApp ? "iOS Development" : "MacOS Development")
+                .padding()
+                .background(self.isDevelopingMacOSApp ? Color.red : Color.green)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+class Model {
+    var value = PassthroughSubject<Bool, Never>()
+    var message = PassthroughSubject<String, Never>()
+    
+    init() {
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 5.0) {
+            self.value.send(true)
+            DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 1.0) {
+                self.message.send("tvOS Development needs to pursue as well.")
+            }
+        }
+    }
+}
+
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
